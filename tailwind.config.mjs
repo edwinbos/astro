02f26270159fc1b9061df60b5667170d424fbeb7
Edwin Bos <edwinbos@users.noreply.github.com>
2024-06-01 20:33:17 +0000

    Added Github block

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		container: {
+            padding: {
+                DEFAULT: '1.5rem',
+                sm: '1rem',
+                md: '1rem',
+                lg: '1.5rem',
+                xl: '1.5rem',
+                '2xl': '1.5rem',
+            },
+            screens: {
+                sm: '100%',
+                md: '100%',
+                lg: '100%',
+                xl: '1280px',
+            },
+        },
+		extend: {
+			borderRadius: {
+				'4xl': '1.875rem'
+			}
+		},
 	},
 	plugins: [],
 }
